7956530fd370d9f6bcbaf75dcca9a8a98bb85dc0
cdavis2223 <cdavis2223@gmail.com>
2023-11-30 01:20:11 +0000

    Segundo commit

--- src/app.js
+++ src/app.js
@@ -6,15 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 function generador() {
-  let quien = ["Mi perro", "Mi abuela", "Mi profesor de 4geeks", "Mi suegra"];
-  let accion = ["se comió", "destruyó", "orinó", "quemó"];
-  let que = ["mi tarea", "mi dignidad", "mi casa", "mi computadora"];
+  let quien = [
+    "Mi perro",
+    "Mi abuela",
+    "Mi profesor de 4geeks",
+    "Mi suegra",
+    "mi compañere de 4 geeks"
+  ];
+  let accion = ["se comió", "destruyó", "orinó", "quemó", "tiró a la basura"];
+  let que = ["mi tarea", "mi dignidad", "mi casa", "mi computadora", "mi ropa"];
   let cuando = [
     "antes de clase",
     "justo a tiempo",
     "cuando venía de camino",
     "mientras dormía",
-    "cuando estaba estudiando"
+    "cuando estaba estudiando",
+    "mientras dormía",
+    "mientras yo estaba bañandome"
   ];
 
   let arrayQuien = Math.floor(Math.random() * quien.length);
