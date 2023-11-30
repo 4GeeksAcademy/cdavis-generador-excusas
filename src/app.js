cf89945cd44d3190428d29559ec1520999eb0c38
cdavis2223 <cdavis2223@gmail.com>
2023-11-30 01:17:06 +0000

    entrega generador de excusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function generador() {
+  let quien = ["Mi perro", "Mi abuela", "Mi profesor de 4geeks", "Mi suegra"];
+  let accion = ["se comió", "destruyó", "orinó", "quemó"];
+  let que = ["mi tarea", "mi dignidad", "mi casa", "mi computadora"];
+  let cuando = [
+    "antes de clase",
+    "justo a tiempo",
+    "cuando venía de camino",
+    "mientras dormía",
+    "cuando estaba estudiando"
+  ];
+
+  let arrayQuien = Math.floor(Math.random() * quien.length);
+  let arrayAccion = Math.floor(Math.random() * accion.length);
+  let arrayQue = Math.floor(Math.random() * que.length);
+  let arrayCuando = Math.floor(Math.random() * que.length);
+  return (
+    quien[arrayQuien] +
+    " " +
+    accion[arrayAccion] +
+    " " +
+    que[arrayQue] +
+    " " +
+    cuando[arrayCuando]
+  );
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = generador();
 };
