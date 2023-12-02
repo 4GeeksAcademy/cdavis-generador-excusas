8d0a0900c5e5f66922cd2962fecf6416f4bf7ca0
Carlos beltran <137569880+DodLitros@users.noreply.github.com>
2023-12-02 01:16:28 +0000

    cambio de nombre de variable

--- src/app.js
+++ src/app.js
@@ -6,37 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 function generador() {
-  let quien = [
-    "Mi perro",
-    "Mi abuela",
-    "Mi profesor de 4geeks",
-    "Mi suegra",
-    "mi compañere de 4 geeks"
-  ];
+  let quien = ["Mi perro", "Mi abuela", "Mi profesor de 4geeks", "Mi suegra", "mi compañere de 4 geeks"];
   let accion = ["se comió", "destruyó", "orinó", "quemó", "tiró a la basura"];
   let que = ["mi tarea", "mi dignidad", "mi casa", "mi computadora", "mi ropa"];
-  let cuando = [
-    "antes de clase",
-    "justo a tiempo",
-    "cuando venía de camino",
-    "mientras dormía",
-    "cuando estaba estudiando",
-    "mientras dormía",
-    "mientras yo estaba bañandome"
-  ];
+  let cuando = ["antes de clase", "justo a tiempo", "cuando venía de camino", "mientras dormía", "cuando estaba estudiando", "mientras dormía", "mientras yo estaba bañandome"];
 
-  let arrayQuien = Math.floor(Math.random() * quien.length);
-  let arrayAccion = Math.floor(Math.random() * accion.length);
-  let arrayQue = Math.floor(Math.random() * que.length);
-  let arrayCuando = Math.floor(Math.random() * que.length);
+  let indiceRandomQuien = Math.floor(Math.random() * quien.length);
+  let indiceRandomAccion = Math.floor(Math.random() * accion.length);
+  let indiceRandomQue = Math.floor(Math.random() * que.length);
+  let indiceRandomCuando = Math.floor(Math.random() * que.length);
   return (
-    quien[arrayQuien] +
+    quien[indiceRandomQuien] +
     " " +
-    accion[arrayAccion] +
+    accion[indiceRandomAccion] +
     " " +
-    que[arrayQue] +
+    que[indiceRandomQue] +
     " " +
-    cuando[arrayCuando]
+    cuando[indiceRandomCuando]
   );
 }
 
